2ea3b62b0d5d6249d70b32a5df84ffad5f81c28d
Butcher <rbituonan@yahoo.com>
2019-02-20 18:56:22 +0800

    new file

---
+++ about.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Hello PHP and HTML</title>
+</head>
+<body>
+<h1>Hello World</h1>
+<?php
+echo "<h2>Hello World</h2>";
+$name="butch";
+echo $name;
+?>
+<script type="text">
+</script>
+</body>
+</html>
